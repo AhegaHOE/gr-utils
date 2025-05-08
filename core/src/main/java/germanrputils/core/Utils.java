0fb1ff7ab39ab0af158eba6abb50def9d5793dcf
AhegaHOE <kratzeisarmy@gmail.com>
2025-05-09 01:14:32 +0200

    Add a utility method to check for legacy addon packets
    Introduce a utility method `isLegacyAddonPacket` to identify legacy addon packets based on `ResourceLocation`. This method supports distinguishing packets tied to "labymod3" and ensures better handling of legacy event identifiers.

---
+++ core/src/main/java/germanrputils/core/Utils.java
@@ -0,0 +1,16 @@
+package germanrputils.core;
+
+import net.labymod.api.client.resources.ResourceLocation;
+
+public final class Utils {
+
+  private Utils() {
+    // Utility class
+  }
+
+  public static boolean isLegacyAddonPacket(ResourceLocation eventIdentifier) {
+    return eventIdentifier.getNamespace().equals("labymod3")
+        && eventIdentifier.getPath().equals("main");
+  }
+
+}
