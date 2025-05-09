9580d923a6a6c68e23d907de150247b43644cc09
AhegaHOE <kratzeisarmy@gmail.com>
2025-05-09 11:15:09 +0200

    Add a handler for server disconnection to reset HUD widgets
    Introduced a listener for `ServerDisconnectEvent` to reset HUD widgets for Heilkrautpflanze and Rose upon server disconnection. This ensures the widgets are cleared appropriately when the server connection is lost, improving consistency and preventing stale data in the UI.

--- core/src/main/java/germanrputils/core/listener/PlantListener.java
+++ core/src/main/java/germanrputils/core/listener/PlantListener.java
@@ -12,6 +12,7 @@ import germanrputils.core.widget.RoseHudWidget;
 import net.labymod.api.event.Subscribe;
 import net.labymod.api.event.client.chat.ChatReceiveEvent;
 import net.labymod.api.event.client.network.server.NetworkPayloadEvent;
+import net.labymod.api.event.client.network.server.ServerDisconnectEvent;
 import net.labymod.api.util.GsonUtil;
 
 import java.util.regex.Matcher;
@@ -19,98 +20,106 @@ import java.util.regex.Pattern;
 
 public class PlantListener {
 
-  private static final Pattern HARVEST_PATTERN = Pattern.compile(
-      "^► Du hast \\w* (\\w*) mit \\d*[x|g] Erlös geerntet\\.$", Pattern.CANON_EQ);
-  private static final String HEILKRAUTPFLANZE_SOW_MESSAGE = "► Du hast eine Heilkrautpflanze ausgelegt.";
-  private static final String ROSE_SOW_MESSAGE = "► Du hast einen Rosenstrauch angepflanzt.";
+    private static final Pattern HARVEST_PATTERN =
+            Pattern.compile("^► Du hast \\w* (\\w*) mit \\d*[x|g] Erlös geerntet\\.$", Pattern.CANON_EQ);
+    private static final String HEILKRAUTPFLANZE_SOW_MESSAGE = "► Du hast eine Heilkrautpflanze ausgelegt.";
+    private static final String ROSE_SOW_MESSAGE = "► Du hast einen Rosenstrauch angepflanzt.";
 
-  private final GRUtilsAddon addon;
-  private final HeilkrautpflanzeHudWidget heilkrautpflanzeHudWidget;
-  private final RoseHudWidget roseHudWidget;
+    private final GRUtilsAddon addon;
+    private final HeilkrautpflanzeHudWidget heilkrautpflanzeHudWidget;
+    private final RoseHudWidget roseHudWidget;
 
-  public PlantListener(final GRUtilsAddon addon,
-      final HeilkrautpflanzeHudWidget heilkrautpflanzeHudWidget,
-      final RoseHudWidget roseHudWidget) {
-    this.addon = addon;
-    this.heilkrautpflanzeHudWidget = heilkrautpflanzeHudWidget;
-    this.roseHudWidget = roseHudWidget;
-  }
-
-  @Subscribe
-  public void onChatReceiveEvent(final ChatReceiveEvent event) {
-    final String message = event.chatMessage().getPlainText();
-
-    if (beginPlantIfSowMessage(message)) {
-      return;
+    public PlantListener(
+            final GRUtilsAddon addon,
+            final HeilkrautpflanzeHudWidget heilkrautpflanzeHudWidget,
+            final RoseHudWidget roseHudWidget
+    ) {
+        this.addon = addon;
+        this.heilkrautpflanzeHudWidget = heilkrautpflanzeHudWidget;
+        this.roseHudWidget = roseHudWidget;
     }
 
-    final Matcher matcher = HARVEST_PATTERN.matcher(message);
+    @Subscribe
+    public void onChatReceiveEvent(final ChatReceiveEvent event) {
+        final String message = event.chatMessage().getPlainText();
 
-    if (!matcher.find()) {
-      return;
-    }
-
-    final String displayName = matcher.group(1);
-    PlantType.fromDisplayName(displayName).ifPresent(plantType -> {
-      switch (plantType) {
-        case HEILKRAUTPFLANZE -> this.heilkrautpflanzeHudWidget.reset();
-        case ROSE -> this.roseHudWidget.reset();
-      }
-    });
-  }
-
-  @Subscribe
-  public void onNetworkPayloadEvent(final NetworkPayloadEvent event) {
-    if (!Utils.isLegacyAddonPacket(event.identifier())) {
-      return;
-    }
-
-    PaketFactory.createPaket(event.getPayload()).ifPresent(paket -> {
-
-      this.addon.logger().debug("Received packet: " + GsonUtil.DEFAULT_GSON.toJson(paket));
-
-      // Ignore unknown pakets
-      if (!(paket instanceof PlantPaket plantPaket)) {
-        return;
-      }
-
-      switch (plantPaket.getType()) {
-        case HEILKRAUTPFLANZE -> this.heilkrautpflanzeHudWidget.onPaketReceive(plantPaket);
-        case ROSE -> this.roseHudWidget.onPaketReceive(plantPaket);
-        default -> {
-          // Ignore unknown packets
+        if (beginPlantIfSowMessage(message)) {
+            return;
         }
-      }
 
-    });
-  }
+        final Matcher matcher = HARVEST_PATTERN.matcher(message);
 
-  /**
-   * Checks if the given message corresponds to a sow action for a specific plant type and
-   * initializes the respective HUD widget if applicable.
-   *
-   * @param message the chat message to be checked for sow actions
-   * @return true if the message corresponds to a sow action and the HUD widget was updated; false
-   * otherwise
-   */
-  private boolean beginPlantIfSowMessage(final String message) {
-    // This is to makes the widget display the plant right after planting it
-    // because the server does not send a packet of the plant until it first ticks
-    switch (message) {
-      case HEILKRAUTPFLANZE_SOW_MESSAGE -> {
-        final Plant plant = PlantFactory.createPlant(PlantType.HEILKRAUTPFLANZE);
-        this.heilkrautpflanzeHudWidget.updatePlant(plant);
-        return true;
-      }
-      case ROSE_SOW_MESSAGE -> {
-        final Plant plant = PlantFactory.createPlant(PlantType.ROSE);
-        this.roseHudWidget.updatePlant(plant);
-        return true;
-      }
-      default -> {
-        return false;
-      }
+        if (!matcher.find()) {
+            return;
+        }
+
+        final String displayName = matcher.group(1);
+        PlantType.fromDisplayName(displayName).ifPresent(plantType -> {
+            switch (plantType) {
+                case HEILKRAUTPFLANZE -> this.heilkrautpflanzeHudWidget.reset();
+                case ROSE -> this.roseHudWidget.reset();
+            }
+        });
     }
-  }
+
+    @Subscribe
+    public void onNetworkPayloadEvent(final NetworkPayloadEvent event) {
+        if (!Utils.isLegacyAddonPacket(event.identifier())) {
+            return;
+        }
+
+        PaketFactory.createPaket(event.getPayload()).ifPresent(paket -> {
+
+            this.addon.logger().debug("Received packet: " + GsonUtil.DEFAULT_GSON.toJson(paket));
+
+            // Ignore unknown pakets
+            if (!(paket instanceof PlantPaket plantPaket)) {
+                return;
+            }
+
+            switch (plantPaket.getType()) {
+                case HEILKRAUTPFLANZE -> this.heilkrautpflanzeHudWidget.onPaketReceive(plantPaket);
+                case ROSE -> this.roseHudWidget.onPaketReceive(plantPaket);
+                default -> {
+                    // Ignore unknown packets
+                }
+            }
+
+        });
+    }
+
+    @Subscribe
+    public void onServerDisconnectEvent(final ServerDisconnectEvent event) {
+        this.heilkrautpflanzeHudWidget.reset();
+        this.roseHudWidget.reset();
+    }
+
+    /**
+     * Checks if the given message corresponds to a sow action for a specific plant type and
+     * initializes the respective HUD widget if applicable.
+     *
+     * @param message the chat message to be checked for sow actions
+     * @return true if the message corresponds to a sow action and the HUD widget was updated; false
+     * otherwise
+     */
+    private boolean beginPlantIfSowMessage(final String message) {
+        // This is to makes the widget display the plant right after planting it
+        // because the server does not send a packet of the plant until it first ticks
+        switch (message) {
+            case HEILKRAUTPFLANZE_SOW_MESSAGE -> {
+                final Plant plant = PlantFactory.createPlant(PlantType.HEILKRAUTPFLANZE);
+                this.heilkrautpflanzeHudWidget.updatePlant(plant);
+                return true;
+            }
+            case ROSE_SOW_MESSAGE -> {
+                final Plant plant = PlantFactory.createPlant(PlantType.ROSE);
+                this.roseHudWidget.updatePlant(plant);
+                return true;
+            }
+            default -> {
+                return false;
+            }
+        }
+    }
 
 }
